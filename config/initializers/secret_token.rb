6e8f133d0abe4643eed1cea6897246444d68bedb
semmin <sbagreev@gmail.com>
2012-03-03 13:20:46 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusLocator::Application.config.secret_token = '402ca7af9c35e7d422ce305ed1a7bb50cf54691e68a45655977ee47534728c370096528911ecac3107a175554e77a5b1e52a3ffc6fe6e49f0702218890d3baca'
