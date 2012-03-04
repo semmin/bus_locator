fb67a02a1df22b566b21030b65463ba4cc8beb3c
semmin <sbagreev@gmail.com>
2012-03-04 15:05:42 -0500

    Changed route assignments

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,18 +1,20 @@
 class BusesController < ApplicationController
   def index
+    @buses = HrtBus::Bus.all
+    @buses.each do |bus|
+      if bus.route_id.blank?
+        ra = RouteAssignment.where(lat: bus.lat, lon: bus.lon).first
+        bus.route_id = ra.route_number if ra.present?
+      end
+    end
   	@buses = HrtBus::Bus.active_buses.select {|bus| bus.route_id == params[:route_id]}
+    @bus = @buses.last if @buses.present?
 
   	if @buses.blank?
   		flash[:notice] = "Sorry, no buses where found on the given route"
   	end
 
-  	@buses.each do |bus|
-	  	File.new("./tmp/map_#{bus.id}.png", "w+") do |f|
-				f.write(bus.static_map)
-				f.save
-				f.close
-			end
-		end
+
   end
 
   def show
